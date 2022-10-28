77e71c8d23ceb0e1afc454c97df753f25ce6743c
AgussOrlov <agusorlov@gmail.com>
2022-10-28 11:42:51 -0300

    Start Proyect

---
+++ login.php
@@ -0,0 +1,51 @@
+<?php session_start() ?>
+<div class="container-fluid">
+	<form action="" id="login-frm">
+		<div class="form-group">
+			<label for="" class="control-label">Email</label>
+			<input type="email" name="email" required="" class="form-control">
+		</div>
+		<div class="form-group">
+			<label for="" class="control-label">Contraseña</label>
+			<input type="password" name="password" required="" class="form-control">
+			<small><a href="javascript:void(0)" class="btn btn-outline-secondary btn-sm" style="margin-top: 0.5rem; font-size: 0.64rem;"  id="new_account">Crear una Cuenta</a></small>
+		</div>
+		<button class="button btn btn-info btn-lg" style=" width: 27.2rem">Login</button>
+	</form>
+</div>
+
+<style>
+	#uni_modal .modal-footer{
+		display:none;
+	}
+</style>
+
+<script>
+	$('#new_account').click(function(){
+		uni_modal("Crea una Cuenta",'signup.php?redirect=index.php?page=checkout')
+	})
+	$('#login-frm').submit(function(e){
+		e.preventDefault()
+		$('#login-frm button[type="submit"]').attr('disabled',true).html('Logging in...');
+		if($(this).find('.alert-danger').length > 0 )
+			$(this).find('.alert-danger').remove();
+		$.ajax({
+			url:'admin/ajax.php?action=login2',
+			method:'POST',
+			data:$(this).serialize(),
+			error:err=>{
+				console.log(err)
+		$('#login-frm button[type="submit"]').removeAttr('disabled').html('Login');
+
+			},
+			success:function(resp){
+				if(resp == 1){
+					location.href ='<?php echo isset($_GET['redirect']) ? $_GET['redirect'] : 'index.php?page=home' ?>';
+				}else{
+					$('#login-frm').prepend('<div class="alert alert-danger">Email o Contraseña Incorrecta...</div>')
+					$('#login-frm button[type="submit"]').removeAttr('disabled').html('Login');
+				}
+			}
+		})
+	})
+</script>
